66b566ec8efc7c1b28741fbf38af2566c178d70c
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2022-10-22 20:50:28 +0600

    environmental variables declare for security

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCbLmzzBtztowS57P-aIHiv-mDqUyaurHs",
-    authDomain: "hussy-news-client.firebaseapp.com",
-    projectId: "hussy-news-client",
-    storageBucket: "hussy-news-client.appspot.com",
-    messagingSenderId: "172088995671",
-    appId: "1:172088995671:web:de25e23f9700af0c7a9812"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
